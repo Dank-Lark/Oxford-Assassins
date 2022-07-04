ba6b3da399d4e7d34bbfd58bd9d35bbb3866e391
Dan Clark <GitHub@nielClark.com>
2022-07-04 21:25:21 +0100

    Base Website changed to one-page layout

--- base/urls.py
+++ base/urls.py
@@ -2,21 +2,9 @@ from django.urls import path
 from . import views
 
 # Base URLs:
-#   home
-#   about
-#   cabal
-#   discord
+#   base
 
 urlpatterns = [
-    # Home page
+    # One page
     path('', views.home, name="home"),
-
-    # About the Guild Page
-    path('about/', views.about, name="about"),
-
-    # Cabal Page
-    path('cabal/', views.cabal, name="cabal"),
-
-    # Discord Page
-    path('discord/', views.discord, name="discord"),
 ]
--- base/views.py
+++ base/views.py
@@ -2,23 +2,8 @@ from urllib import request
 from django.shortcuts import render
 
 # Base Views:
-#   home
-#   about
-#   cabal
-#   discord
+#   base
 
 def home(request):
     context = {}
-    return render(request, 'base/home.html', context)
-
-def about(request):
-    context = {}
-    return render(request, 'base/about.html', context)
-
-def cabal(request):
-    context = {}
-    return render(request, 'base/cabal.html', context)
-
-def discord(request):
-    context = {}
-    return render(request, 'base/discord.html', context)
+    return render(request, 'base/base.html', context)
